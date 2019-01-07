32fa04c63a15191da859e3f338b77b3a2a7b26c1
lissoff2606 <lissoff2606@gmail.com>
2019-01-07 23:45:35 +0400

    Init commit

--- src/Bubble.java
+++ src/Bubble.java
@@ -21,8 +21,8 @@ public class Bubble {
 }
 /*основы git
 git init                                                    инициализация в нашем проекте
-git config --global user.name "lissoff2606"                 инициализация имени
-git config --global user.email "lissoff2606@gmail.com"      инициализация адреса
+git config --global user.name "name"                        инициализация имени
+git config --global user.email "@gmail.com"                 инициализация адреса
 git status                                                  статус проекта (отслеживаемые файлы)
 создание файла .gitignore в корне проекта . в этом файле указываем файлы , которые мы не хотим отслеживать в нашей системе контроля версий (.idea/*    out/*   *.iml)
 git add --all                                               добавление файлов
